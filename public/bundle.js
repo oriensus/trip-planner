4797b7ee711a5a989ea3cd70179e776eceacf852
Josh Luria <josh.luria@gmail.com>
2017-11-21 13:00:45 -0500

    Add a marker to the focus location of the map

--- public/bundle.js
+++ public/bundle.js
@@ -78,6 +78,13 @@ const map = new mapboxgl.Map({
   style: 'mapbox://styles/mapbox/streets-v10'
 });
 
+const markerDomEl = document.createElement('div');
+markerDomEl.style.width = '32px';
+markerDomEl.style.height = '39px';
+markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)';
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009151, 40.705086]).addTo(map);
+
 
 /***/ }),
 /* 1 */
--- src/index.js
+++ src/index.js
@@ -8,3 +8,10 @@ const map = new mapboxgl.Map({
   zoom: 12,
   style: 'mapbox://styles/mapbox/streets-v10'
 });
+
+const markerDomEl = document.createElement('div');
+markerDomEl.style.width = '32px';
+markerDomEl.style.height = '39px';
+markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)';
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009151, 40.705086]).addTo(map);
