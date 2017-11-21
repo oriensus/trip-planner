e6b055ce28868672d5c639876915246ebc4eb1c8
Josh Luria <josh.luria@gmail.com>
2017-11-21 12:50:01 -0500

    Add mapbox components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('ddddddddd');
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoiamx1cmlhIiwiYSI6ImNqYTl3YmhpaTBsaTcyd3E5anl5ejc1NHoifQ.Wdl8hoLAGP0HMqs2Vk4o4w';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [-74.009, 40.705],
+  zoom: 12,
+  style: 'mapbox://styles/mapbox/streets-v10'
+});
